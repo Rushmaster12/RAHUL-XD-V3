e88131225bb14e6e1d5efeacad0ab92d48b6d3a9
RAHUL-MASTER <rahulhiran37@gmail.com>
2025-02-02 17:40:01 +0530

    Update news.js

--- plugins/news.js
+++ plugins/news.js
@@ -24,7 +24,7 @@ async (conn, mek, m, { from, reply }) => {
 ⚠️ _${article.description}_
 🔗 _${article.url}_
 
-  *©ᴘᴏᴡᴇʀᴇᴅ ᴀᴄᴅ-ᴍᴅ*
+  *©ᴘᴏᴡᴇʀᴇᴅ RAHUL-XD-V3*
             `;
 
             console.log('Article URL:', article.urlToImage); // Log image URL for debugging
