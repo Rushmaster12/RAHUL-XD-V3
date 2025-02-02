adaee12a155588c200dbf23a4babd1c6d18310da
Rushmaster12 <160727728+Rushmaster12@users.noreply.github.com>
2025-02-02 21:27:06 +0530

    Create WhatsApp.js

---
+++ plugins/WhatsApp.js
@@ -0,0 +1,16 @@
+System({
+    pattern: "pp$",
+    fromMe: true,
+    type: "whatsapp",
+    alias: ['fullpp', 'setpp'],
+    desc: "Set full screen profile picture",
+}, async (message, match) => {
+    if (match === "remove") {
+        await message.client.removeProfilePicture(message.user.jid);
+        return await message.reply("_Profile Picture Removed_");
+    }
+    if (!message.reply_message || !message.reply_message.image) return await message.reply("_Reply to a photo_");
+    let media = await message.reply_message.download();
+    await message.client.updateProfile(media, message.user.jid);
+    return await message.reply("_Profile Picture Updated POWERED BY RUSH MASTER!_");
+});
