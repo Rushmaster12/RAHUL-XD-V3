fc2604aefc98ac01995a586053ce0e066462d433
RAHUL-MASTER <rahulhiran37@gmail.com>
2025-02-02 18:11:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "2VVSSToI#XbauT9Vb0dl9827ui3fFqUYELdMiLvVA0kdQza6b5yI", //Put your session code here
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/h2qai0.jpg",ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/h2qai0.jpg",
-ALIVE_MSG: process.env.ALIVE_IMG || "*HEY DEAR* ${pushname}\n *I Am Rahul-xd-v3 Alive No☑️🖐🏻*",
+ALIVE_MSG: process.env.ALIVE_IMG || "*HEY DEAR* ${pushname}\n *I Am Rahul-xd-v3 Alive Now☑️🖐🏻*",
 SUDO_NB: process.env.SUDO_NB || "919356730236",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
