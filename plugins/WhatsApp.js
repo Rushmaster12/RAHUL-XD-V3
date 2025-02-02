dbde78f7a46c153cbb9ab90057abb8a512f0b1c6
RAHUL-MASTER <rahulhiran37@gmail.com>
2025-02-02 23:45:49 +0530

    Update WhatsApp.js

--- plugins/WhatsApp.js
+++ plugins/WhatsApp.js
@@ -12,5 +12,5 @@ System({
     if (!message.reply_message || !message.reply_message.image) return await message.reply("_Reply to a photo_");
     let media = await message.reply_message.download();
     await message.client.updateProfile(media, message.user.jid);
-    return await message.reply("_Profile Picture Updated POWERED BY RUSH MASTER!_");
+    return await message.reply("_Profile Picture Updated POWERED BY RAHUL-MASTER!_");
 });
