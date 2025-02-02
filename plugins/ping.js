144acf64f68e641603f5fb19637230723c90047a
RAHUL-MASTER <rahulhiran37@gmail.com>
2025-02-02 17:46:18 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -5,16 +5,16 @@ cmd({
     pattern: "ping",
     desc: "Check bot's response time.",
     category: "main",
-    react: "✅",
+    react: "🔌",
     filename: __filename
 },
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
         const startTime = Date.now()
-        const message = await conn.sendMessage(from, { text: '*ᴀᴄᴅ-ᴍᴅ...*' })
+        const message = await conn.sendMessage(from, { text: '*RAHUL-XD-V3*' })
         const endTime = Date.now()
         const ping = endTime - startTime
-        await conn.sendMessage(from, { text: `*📍 Ping : ${ping}ms*` }, { quoted: message })
+        await conn.sendMessage(from, { text: `*🌈🌟ping : ${ping}ms*` }, { quoted: message })
     } catch (e) {
         console.log(e)
         reply(`${e}`)
